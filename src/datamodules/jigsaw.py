9e1c7933608f46981a28172ac86f826470b0ceac
Minh Duc Bui <duc@Minhs-MacBook-Pro.local>
2023-09-24 14:59:12 +0200

    formatted metric output

--- src/datamodules/jigsaw.py
+++ src/datamodules/jigsaw.py
@@ -31,19 +31,22 @@ class JigsawDataModule(BaseDataModule):
     @property
     def num_labels(self) -> int:
         return 2
-    
-    def setup(self, stage: Optional[str] = None):
-        log.info("Check Data...")
-        dataset = self.load_dataset(self.train_data_path)
-        dataset = dataset.train_test_split(test_size=0.2,
-                                           shuffle=True,
-                                           seed=42)
 
-        self.data_train = dataset["train"]
-        self.data_val = dataset["test"]
-        dataset = self.load_dataset(self.test_data_path)
-        self.data_test = dataset
+    def setup(self, stage: str):
+        # Assign train/val datasets for use in dataloaders
+        if stage == "fit":
+            log.info("Check Data...")
+            dataset = self.load_dataset(self.train_data_path)
+            dataset = dataset.train_test_split(test_size=0.2,
+                                               shuffle=True,
+                                               seed=42)
 
+            self.data_train = dataset["train"]
+            self.data_val = dataset["test"]
+        # Assign test dataset for use in dataloader(s)
+        if stage == "test":
+            dataset = self.load_dataset(self.test_data_path)
+            self.data_test = dataset
 
     def prepare_data(self):
         """Use this method to do things that might write to disk or that need to
@@ -54,14 +57,14 @@ class JigsawDataModule(BaseDataModule):
 
         # Get Training Data from cc100 for MLM
         log.info("Check Data...")
-        
+
         if os.path.exists(self.train_data_path) and os.path.exists(self.test_data_path):
             log.info(f"The dataset files exists.")
         else:
             raise FileNotFoundError(f"The dataset files do not exist.")
-    
 
     # TODO: Move to Collator
+
     def load_dataset(self, path):
         log.warning("Load CSV File...")
         df = pd.read_csv(path)
@@ -76,7 +79,7 @@ class JigsawDataModule(BaseDataModule):
         df["labels"] = (df["labels"] >= threshold).astype(float)
         # Create a Hugging Face dataset from the modified DataFrame
         dataset = Dataset.from_pandas(df)
-        
+
         # https://github.com/huggingface/datasets/issues/2583
         # language_dataset = language_dataset.with_format("torch")
         # dataset = dataset.with_format("torch")
@@ -85,26 +88,27 @@ class JigsawDataModule(BaseDataModule):
         dataset = dataset.map(self.tokenization)
         dataset = dataset.remove_columns(["text"])
         return dataset
-    
+
     def tokenization(self, example):
         return self.tokenizer(example["text"], truncation=True, padding=False, return_token_type_ids=True)
-
 
     def create_coarsed_grained_annotation(self, df, drop_irrelevant_columns=True):
         MAPPING = {"religion": ["atheist", "buddhist", "muslim", "christian", "hindu", "jewish", "other_religion"],
                    "race": ["white", "asian", "black", "latino", "other_race_or_ethnicity"],
                    "gender_and_sexual_orientation": ["bisexual", "female", "male", "heterosexual",
-                                                     "homosexual_gay_or_lesbian", "transgender", "other_gender", 
+                                                     "homosexual_gay_or_lesbian", "transgender", "other_gender",
                                                      "other_sexual_orientation"]}
-        
-        result = pd.DataFrame({key: (df[MAPPING[key]] >= 0.5).any(axis=1).astype(int) for key in MAPPING})
+
+        result = pd.DataFrame({key: (df[MAPPING[key]] >= 0.5).any(
+            axis=1).astype(int) for key in MAPPING})
         df_combined = pd.concat([df, result], axis=1)
-        
+
         # For Test Set
         if "toxicity" in df_combined:
             df_combined = df_combined.rename(columns={"toxicity": "target"})
-        
+
         if drop_irrelevant_columns:
-            df_combined = df_combined[["comment_text", "target", "religion", "race", "gender_and_sexual_orientation"]]
+            df_combined = df_combined[[
+                "comment_text", "target", "religion", "race", "gender_and_sexual_orientation"]]
 
         return df_combined
--- src/evaluation/equalized_odds.py
+++ src/evaluation/equalized_odds.py
@@ -9,7 +9,7 @@ class EqualizedOdds(Metric):
         # dist_reduce_fx indicates the function that should be used to reduce
         # state from multiple processes
         super().__init__(dist_sync_on_step=dist_sync_on_step)
-        
+
         self.mapping = {}
         for index, name in enumerate(attribute_names):
             self.mapping[index] = name
@@ -17,16 +17,14 @@ class EqualizedOdds(Metric):
         self.add_state("target", default=[], dist_reduce_fx=None)
         self.add_state("sensitive_attributes", default=[], dist_reduce_fx=None)
 
-
     def update(self, preds, target, religion, race, gender_and_sexual_orientation):
-        self.sensitive_attributes = torch.vstack((religion, race, gender_and_sexual_orientation)).T
+        self.sensitive_attributes = torch.vstack(
+            (religion, race, gender_and_sexual_orientation)).T
         self.preds = preds
         self.target = target
 
-
     def compute(self):
         return self._compute(self.preds, self.target, self.sensitive_attributes)
-        
 
     def compute_deo_for_multi_label_loop(self, predictions, references, sensitive_attributes):
         '''compute deo based on loops
@@ -35,7 +33,6 @@ class EqualizedOdds(Metric):
         assert len(references) != 0
         assert len(predictions) == len(references) == len(sensitive_attributes)
 
-        unique_y_values = torch.unique(torch.vstack((predictions, references))).tolist()
         num_a_values = sensitive_attributes.shape[1]
 
         group_fpr_dict = {}
@@ -50,22 +47,30 @@ class EqualizedOdds(Metric):
             # Calculate the total positive/negative examples for the sensitive group
             group_pos_label_ids = (references == y) & (group_id)
             group_neg_label_ids = (references != y) & (group_id)
-            group_pos = torch.sum(group_pos_label_ids) if torch.sum(group_pos_label_ids) != 0 else 0
-            group_neg = torch.sum(group_neg_label_ids) if torch.sum(group_neg_label_ids) != 0 else 0
+            group_pos = torch.sum(group_pos_label_ids) if torch.sum(
+                group_pos_label_ids) != 0 else 0
+            group_neg = torch.sum(group_neg_label_ids) if torch.sum(
+                group_neg_label_ids) != 0 else 0
 
             # Calculate the total positive/negative examples for the complementary group
-            group_pos_label_ids_com = (references == y) & (sensitive_attributes[:, a_idx] == 0)
-            group_neg_label_ids_com = (references != y) & (sensitive_attributes[:, a_idx] == 0)
-            group_pos_com = torch.sum(group_pos_label_ids_com) if torch.sum(group_pos_label_ids_com) != 0 else 0
-            group_neg_com = torch.sum(group_neg_label_ids_com) if torch.sum(group_neg_label_ids_com) != 0 else 0
+            group_pos_label_ids_com = (references == y) & (
+                sensitive_attributes[:, a_idx] == 0)
+            group_neg_label_ids_com = (references != y) & (
+                sensitive_attributes[:, a_idx] == 0)
+            group_pos_com = torch.sum(group_pos_label_ids_com) if torch.sum(
+                group_pos_label_ids_com) != 0 else 0
+            group_neg_com = torch.sum(group_neg_label_ids_com) if torch.sum(
+                group_neg_label_ids_com) != 0 else 0
 
             # Calculate TP/FP for the sensitive group
             group_tp_ids = (references == y) & (predictions == y) & (group_id)
             group_fp_ids = (references != y) & (predictions == y) & (group_id)
 
             # Calculate TP/FP for the complementary group
-            group_tp_ids_com = (references == y) & (predictions == y) & (sensitive_attributes[:, a_idx] == 0)
-            group_fp_ids_com = (references != y) & (predictions == y) & (sensitive_attributes[:, a_idx] == 0)
+            group_tp_ids_com = (references == y) & (
+                predictions == y) & (sensitive_attributes[:, a_idx] == 0)
+            group_fp_ids_com = (references != y) & (
+                predictions == y) & (sensitive_attributes[:, a_idx] == 0)
 
             group_tpr = torch.sum(group_tp_ids) / group_pos
             group_fpr = torch.sum(group_fp_ids) / group_neg
@@ -79,10 +84,10 @@ class EqualizedOdds(Metric):
             if sum(group_id) != 0:
                 preds_group = predictions[group_id]
                 reference_group = references[group_id]
-                balanced_accuracy[(1, a_idx)] = accuracy(preds_group, reference_group, 
-                                                    task="multiclass",
-                                                    num_classes=2,
-                                                    average="macro")
+                balanced_accuracy[(1, a_idx)] = accuracy(preds_group, reference_group,
+                                                         task="multiclass",
+                                                         num_classes=2,
+                                                         average="macro")
             else:
                 balanced_accuracy[(1, a_idx)] = 0
 
@@ -94,18 +99,15 @@ class EqualizedOdds(Metric):
 
         return group_metric_rates
 
-
     def _get_unique_labels(self, labels):
         unique_labels = torch.unique(labels)
         return unique_labels.tolist()
-
 
     def _compute(self, predictions, references, sensitive_attributes):
         ''' Actual Implementation of compute metrics
         '''
 
         # get cardinality of Y and A
-        unique_y_values = torch.unique(torch.vstack((predictions, references))).tolist()
         num_a_values = torch.tensor(sensitive_attributes).shape[1]
 
         # EO Calculation
@@ -115,22 +117,20 @@ class EqualizedOdds(Metric):
             sensitive_attributes=sensitive_attributes,
         )
 
-        # by group metrics (could be extended when a is not binary)
-        fprs_diff, fnrs_diff = [], []
-        tprs_diff, tnrs_diff = [], []
-
         eo_per_attribute = []
         for a_idx in range(num_a_values):
-            tpr_diff = torch.abs(group_metric_rates['tpr'][(1, a_idx)] - group_metric_rates['tpr'][(0, a_idx)])
-            fpr_diff = torch.abs(group_metric_rates['fpr'][(1, a_idx)] - group_metric_rates['fpr'][(0, a_idx)])
+            tpr_diff = torch.abs(group_metric_rates['tpr'][(
+                1, a_idx)] - group_metric_rates['tpr'][(0, a_idx)])
+            fpr_diff = torch.abs(group_metric_rates['fpr'][(
+                1, a_idx)] - group_metric_rates['fpr'][(0, a_idx)])
             eo_per_attribute.append(torch.max(tpr_diff, fpr_diff))
-            
+
         # Initialize an empty dictionary
         metrics = {}
         # Iterate through the list and create key-value pairs
         for index, item in enumerate(eo_per_attribute):
             metrics["eo/" + self.mapping[index]] = item
-            
+
         unpacked_dict = {}
         for key, value in group_metric_rates.items():
             for subkey, subvalue in value.items():
@@ -138,8 +138,7 @@ class EqualizedOdds(Metric):
                     groupname = "_comp"
                 else:
                     groupname = ""
-                new_key = key + groupname +  "_" + self.mapping[subkey[1]]
+                new_key = key + "/" + self.mapping[subkey[1]] + groupname
                 unpacked_dict[new_key] = subvalue
-        #for key, values in group_metric_rates.items():
-        #    metrics[key + "_" + self.mapping[index]] = values
+
         return {**unpacked_dict, **metrics}
